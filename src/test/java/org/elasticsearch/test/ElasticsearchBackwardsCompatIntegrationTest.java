05419651b969c4a0ae4fe1b67399072ff1f7a666
javanna <cavannaluca@gmail.com>
2014-08-29 19:28:30 +0200

    [TEST] Make sure random index template uses the proper number of data nodes in bw comp tests
    When creating indices with no specific number of replicas set, the random index template is used, which has a maximum number of replicas set to the total number of data nodes - 1.
    That works well in usual tests, but not for bw comp tests as we have an allocation decider that prevents allocation of replicas when the primary is allocated on a newer node. That means that replicas are not allocated and in some cases there's no quorum and index operations time out.

--- src/test/java/org/elasticsearch/test/ElasticsearchBackwardsCompatIntegrationTest.java
+++ src/test/java/org/elasticsearch/test/ElasticsearchBackwardsCompatIntegrationTest.java
@@ -129,7 +129,7 @@ public abstract class ElasticsearchBackwardsCompatIntegrationTest extends Elasti
     protected int maximumNumberOfReplicas() {
         //NodeVersionAllocationDecider prevents replicas from being allocated if the primary is on a newer node
         //that's why we only take into account data nodes that belong to the internal cluster
-        return backwardsCluster().internalCluster().numDataNodes() - 1;
+        return Math.max(0, backwardsCluster().internalCluster().numDataNodes() - 1);
     }
 
     @Before
