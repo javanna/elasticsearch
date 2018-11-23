b5044587efe88d10d8e3ecf6afe899129c34c57f
javanna <cavannaluca@gmail.com>
2018-11-23 21:52:12 +0100

    Revert "Revert "add a backdoor to simulate some configurable latency""
    This reverts commit 1900b5fa9178e5a089f59d50aeae1114c9a5657b.

--- server/src/main/java/org/elasticsearch/common/settings/ClusterSettings.java
+++ server/src/main/java/org/elasticsearch/common/settings/ClusterSettings.java
@@ -443,7 +443,8 @@ public final class ClusterSettings extends AbstractScopedSettings {
                     Node.BREAKER_TYPE_KEY,
                     OperationRouting.USE_ADAPTIVE_REPLICA_SELECTION_SETTING,
                     IndexGraveyard.SETTING_MAX_TOMBSTONES,
-                    EnableAssignmentDecider.CLUSTER_TASKS_ALLOCATION_ENABLE_SETTING
+                    EnableAssignmentDecider.CLUSTER_TASKS_ALLOCATION_ENABLE_SETTING,
+                    TcpTransport.TRANSPORT_TCP_RESPONSE_LATENCY
             )));
 
     public static List<SettingUpgrader<?>> BUILT_IN_SETTING_UPGRADERS = Collections.unmodifiableList(Arrays.asList(
