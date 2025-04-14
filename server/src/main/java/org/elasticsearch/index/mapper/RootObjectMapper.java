094e2df024797fdd5d123d99b57c7df8a741508f
Luca Cavanna <javanna@apache.org>
2025-04-14 17:00:09 +0200

    wip

--- server/src/main/java/org/elasticsearch/index/mapper/RootObjectMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/RootObjectMapper.java
@@ -540,4 +540,20 @@ public class RootObjectMapper extends ObjectMapper {
     public int getTotalFieldsCount() {
         return super.getTotalFieldsCount() - 1 + runtimeFields.size();
     }
+
+    private static final String RESERVED_NAMESPACE = "_project";
+
+    @Override
+    protected void validateSubField(Mapper mapper, MappingLookup mappers) {
+        if (subobjects() == Subobjects.ENABLED) {
+            if (mapper.leafName().equals(RESERVED_NAMESPACE)) {
+                throw new IllegalArgumentException("reserved namespace");
+            }
+        } else {
+            if (mapper.leafName().startsWith(RESERVED_NAMESPACE)) {
+                throw new IllegalArgumentException("reserved namespace");
+            }
+        }
+        super.validateSubField(mapper, mappers);
+    }
 }
